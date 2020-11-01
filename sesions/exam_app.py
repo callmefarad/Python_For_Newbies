6d2b6a62e5b472b6fffcf86133373247d0f96fd0
callmefarad <65671589+callmefarad@users.noreply.github.com>
2020-11-01 13:28:45 +0100

    Added new field in Pretty table
    1/11/2020

--- sesions/exam_app.py
+++ sesions/exam_app.py
@@ -3,15 +3,15 @@ from exam_mod import Exam, run_app
 
 # creating a list of questions 
 question_list = [
-    "(1)    whats the name of your python instructor?\n(a) ubani\n(b) peter\n(c) bukola\n\n",
-    "(2)    an application on a tablet is known as _____?\n(a) web app\n(b) mobile app\n(c) desktop app\n\n",
-    "(3)    sum is used to subtract values.\n(a) True\n(b) None\n(c) False\n\n",
-    "(4)    django is a ____?\n(a) programming language\n(b) framework\n(c) IDE\n\n",
-    "(5)    ____ is used to access the value in dictionary?\n(a) key\n(b) id\n(c) class\n\n",
+    "(1) whats the name of your python instructor?\n(a) ubani\n(b) peter\n(c) bukola\n\n",
+    "(2) an application on a tablet is known as _____?\n(a) web app\n(b) mobile app\n(c) desktop app\n\n",
+    "(3) sum is used to subtract values.\n(a) True\n(b) None\n(c) False\n\n",
+    "(4) django is a ____?\n(a) programming language\n(b) framework\n(c) IDE\n\n",
+    "(5) ____ is used to access the value in dictionary?\n(a) key\n(b) id\n(c) class\n\n",
 ]
 
 
-# creating a list (question) of instances of the class "Exam" in the exam.mod
+# creating a list (question) instances of the class "Exam" in the exam.mod
 question = [
     Exam(question_list[0], 'a'),
     Exam(question_list[1], 'b'),
--- sesions/exam_mod.py
+++ sesions/exam_mod.py
@@ -2,7 +2,7 @@ from prettytable import PrettyTable
 
 # creating a class for the application
 class Exam:
-    """ DOCUMENTATION: Exam class takes 2 parameters: (question(String), answer(String)). """
+    """ DOCUMENTATION: Exam class takes 2 arguments: (question(String), answer(String)). """
     # construct of the class
     def __init__(self, question, answer):
         self.question = question
@@ -12,7 +12,7 @@ class Exam:
 
 # create a function to run the application
 def run_app(question_list):
-    """ DOCUMENTATION: run_app() functions takes 1 parameter. (question_list(String)) """
+    """ DOCUMENTATION: run_app() functions takes 1 argument. (question_list(String)) """
     # Ask the user to prompt his/her name
     name = input("What is your name? ")
     score = 0
@@ -22,20 +22,24 @@ def run_app(question_list):
             score += 1
     if score < 3:
         remark = "Failed"
-    else:
+    elif score > 3 and score < 5:
         remark = "Passed"
+    else:
+        remark = "Excellent"
 
     # store the estimated value of score to a variable named result
-    result = score, '/', len(question_list)
+    result = score
+    # total questions
+    total = len(question_list)
     #creating an instance of class prettyTable()
     x = PrettyTable()
     # creating the fields i.e columns of the table
-    x.field_names = ['Names', 'Score', 'Remark']
-    # add values to the table
-    x.add_row([name, result, remark])
+    x.field_names = ['Names', 'Score', 'Total Score', 'Remark']
+    # add values to the table fields created
+    x.add_row([name, result, total, remark])
     # print all record in a tabular format
     print(x)
     print("\n")
-    print("Name: ", name, "\nScore: ", score, "/", len(question_list), "\nRemark: ", remark)
+    print("Name: ", name, "\nScore: ", score, "\nTotal Score: ", len(question_list), "\nRemark: ", remark)
 
 
